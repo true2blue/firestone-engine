0e5cdcd456e463b5b6fd2b7e1bf1163d977a4c5f
true2blue <842958037@qq.com>
2024-01-10 21:12:05 +0800

    fix trade price

--- src/firestone_engine/Real.py
+++ src/firestone_engine/Real.py
@@ -208,7 +208,7 @@ class Real(object):
     def createOrder(self):
         data = self.get_data()[-1]
         code = self.get_code()
-        price = float(data['price'])
+        price = float("{:.2f}".format(float(data['pre_close']) * 1.1))
         Real._logger.info(f'start create order for code = {code}, time = {data["time"]}')
         op = self.get_op()
         if(op == 'buy'):
