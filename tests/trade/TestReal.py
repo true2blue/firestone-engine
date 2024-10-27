48c5e27b86b1ce711093d5b09d70ff8b15d06e48
true2blue <842958037@qq.com>
2024-10-27 14:39:10 +0800

    trade

--- tests/trade/TestReal.py
+++ tests/trade/TestReal.py
@@ -6,22 +6,22 @@ class TestReal(unittest.TestCase):
 
 
     def setUp(self):
-        self.real = Real('6220bddd91fe522cec3db244', date='2021-10-13')
+        self.real = Real('671ddee64f592501c8d5e82b', date='2024-10-27')
         self.real.data = {
             'data' : [{
-                    'name' : '浙大网新'
+                'name' : '人工智能ETF'
             }],
         }
 
 
-    # def test_create_delegate(self):
-    #     print(self.real.createDelegate('600797', 6.54, 100, 'buy'))
+    def test_create_delegate(self):
+        print(self.real.createDelegate('159819', 0.881, 100, 'buy'))
 
-    # def test_query_chengjiao(self):
-    #     print(self.real.queryChenjiao('94584'))
+    def test_query_chengjiao(self):
+        print(self.real.queryChenjiao('14186'))
 
-    # def test_cancel_delegate(self):
-    #     print(self.real.cancelDelegate('891', '20211024'))
+    def test_cancel_delegate(self):
+        print(self.real.cancelDelegate('14186', '20241027'))
 
 if __name__ == "__main__":
     #     # 5678 is the default attach port in the VS Code debug configurations
