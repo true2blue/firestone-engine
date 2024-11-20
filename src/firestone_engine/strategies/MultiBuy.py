616271759c81ecf3df96d6ebce441db8fd8fc4db
true2blue <842958037@qq.com>
2024-11-20 19:36:02 +0800

    fix multibuy

--- src/firestone_engine/strategies/MultiBuy.py
+++ src/firestone_engine/strategies/MultiBuy.py
@@ -26,7 +26,7 @@ class MultiBuy(Base):
         limit_open_percent = float(self.trade['params']['limit_open_percent'])
         if percent < limit_open_percent:
             self.pre_buy = close
-            MultiBuy._logger.info(f'match_first_buy open_percent = {percent}, limit_open_percent = {limit_open_percent}')
+            MultiBuy._logger.info(f'match_first_buy open_percent = {percent}, limit_open_percent = {limit_open_percent}, close = {close}')
             return True
         return False
     
@@ -39,7 +39,7 @@ class MultiBuy(Base):
         percent = (self.pre_buy - close) / pre_close * 100
         drop_open_percent = float(self.trade['params']['drop_open_percent']) * self.buy_count
         if percent > drop_open_percent:
-            MultiBuy._logger.info(f'match_next_buy {self.buy_count} drop percent from open = {percent}, drop_open_percent = {drop_open_percent}')
+            MultiBuy._logger.info(f'match_next_buy {self.buy_count} drop percent from open = {percent}, drop_open_percent = {drop_open_percent}, pre_buy = {self.pre_buy}, close = {close}')
             return True
         return False
     
