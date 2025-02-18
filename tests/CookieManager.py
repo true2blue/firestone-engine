37e4212eeeb7282dab331ee6dce9e18e8cbe3652
Benjamin Li <842958037@qq.com>
2025-02-18 10:25:02 +0800

    add cookie

---
+++ tests/CookieManager.py
@@ -0,0 +1,24 @@
+import browser_cookie3
+from pymongo import MongoClient
+import os
+
+class CookieManager(object):
+
+    _MONFO_URL = '127.0.0.1'
+
+    def __init__(self):
+        self.client = MongoClient(CookieManager._MONFO_URL, 27017)
+        self.db = self.client[os.environ['FR_DB']]
+
+    def run(self):
+        cj = browser_cookie3.chrome(domain_name='github.com')
+        cookie_str = ''
+        for cookie in cj:
+            cookie_str += f"{cookie.name}={cookie.value}; "
+        configs = self.db['configs'].find({})
+        for config in configs:
+            self.db['configs'].update_one({"_id" : config['_id']},{"$set" : {'cookie' : cookie_str}})
+
+if __name__ == '__main__':
+    manager = CookieManager()
+    manager.run()
