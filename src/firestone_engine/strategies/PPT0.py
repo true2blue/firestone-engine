290f667e42a2815bb0d644aedc374fa5e3123d70
Benjamin Li <842958037@qq.com>
2025-03-14 09:49:46 +0800

    cb

--- src/firestone_engine/strategies/PPT0.py
+++ src/firestone_engine/strategies/PPT0.py
@@ -60,7 +60,7 @@ class PPT0(Base):
         low_limit = Utils.round_dec(pre_close * 0.9, self.precise())
         cb = float(self.trade['params']['cb'])
         buy_below_cb_percent = float(self.trade['params']['buy_below_cb_percent'])
-        drop_from_close_percent = (cb - close) / pre_close * 100
+        drop_from_close_percent = (cb - close) / cb * 100
         if drop_from_close_percent < buy_below_cb_percent:
             return False
         if percent > float(self.trade['params']['drop_percent']) * -1:
